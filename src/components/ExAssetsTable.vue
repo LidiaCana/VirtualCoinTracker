c720b644712b2a47f0fa0714ba09f1441e45602f
Lidia <in.canaless@gmail.com>
2020-04-09 01:17:51 -0600

    Tracker virtual coin in vue

---
+++ src/components/ExAssetsTable.vue
@@ -0,0 +1,116 @@
+<template>
+  <table>
+    <thead>
+      <tr class="bg-gray-100 border-b-2 border-gray-400">
+        <th></th>
+        <th>
+          <span>Ranking</span>
+        </th>
+        <th>Nombre</th>
+        <th>Precio</th>
+        <th>Cap. de Mercado</th>
+        <th>Variación 24hs</th>
+        <td class="hidden sm:block"></td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="a in assets"
+        :key="a.d"
+        class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
+      >
+        <td>
+          <img
+            class="w-6 h-6"
+            :src="
+              `https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`
+            "
+            :alt="a.name"
+          />
+        </td>
+        <td>
+          <b>{{ a.rank }}</b>
+        </td>
+        <td>
+          <router-link
+            class="hover: underline text-green-600"
+            :to="{ name: 'coin-detail', params: { id: a.id } }"
+            >{{ a.name }}</router-link
+          >
+          <small class="ml-1 text-gray-500">{{ a.symbol }}</small>
+        </td>
+        <td>{{ a.priceUsd | dollar }}</td>
+        <td>{{ a.marketCapUsd | dollar }}</td>
+        <td
+          :class="
+            a.changePercent24Hr.includes('-')
+              ? 'text-red-400'
+              : 'text-green-400'
+          "
+        >
+          {{ a.changePercent24Hr | percent }}
+        </td>
+        <td class="hidden sm:block">
+          <ex-button @custon-click="getCoin(a.id)">
+            <span>Detalle</span>
+          </ex-button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import ExButton from "@/components/ExButton";
+
+export default {
+  name: "PxAssetsTable",
+  components: {
+    ExButton
+  },
+  methods: {
+    getCoin(id) {
+      this.$router.push({ name: "coin-detail", params: { id } });
+    }
+  },
+  props: {
+    assets: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
+
+<style scoped>
+.up::before {
+  content: "👆";
+}
+
+.down::before {
+  content: "👇";
+}
+
+td {
+  padding: 20px 0px;
+  font-size: 0.6rem;
+  text-align: center;
+}
+
+th {
+  padding: 5px;
+  font-size: 0.6rem;
+}
+
+@media (min-width: 640px) {
+  td,
+  th {
+    padding: 20px;
+    font-size: 1rem;
+  }
+
+  th {
+    padding: 12px;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100"/>
+    <ex-assets-table v-if="!isLoading" :assets="assets"></ex-assets-table>
+  </div>
+</template>
+<script>
+import ExAssetsTable from "@/components/ExAssetsTable";
+import api from "@/api";
+export default {
+  name: "Home",
+  components: {
+    ExAssetsTable
+  },
+  data() {
+    return {
+      isLoading: false,
+      assets: []
+    };
+  },
+
+  created() {
+    this.isLoading = true;
+    api
+      .getAssets()
+      .then(assets => (this.assets = assets))
+      .finally(() => (this.isLoading = false));
+  }
+};
+</script>
